aed95276206a4d8d6c64715faf7e7d5dc0a81246
github username <github email address>
2015-08-21 15:13:49 -0400

    not quite done, dynamic numbering, needs to pull up yelp

--- mainfile.rb
+++ mainfile.rb
@@ -16,11 +16,9 @@ class Location
 		@result = JSON.load(@result)
 		@address = @result["results"][0]["formatted_address"]
 		@longitude= @result["results"][0]["geometry"]["location"]["lng"].to_f	
-		@latitude = @result["results"][0]["geometry"]["location"]["lat"].to_f
-		#binding.pry	
+		@latitude = @result["results"][0]["geometry"]["location"]["lat"].to_f	
 	end 
 end
-
 
 class Search
 	attr_reader :wanted, :radius
@@ -36,7 +34,6 @@ class Search
 		@radius= gets.chomp
 		@radius = ((@radius.to_f)*1609.34).to_s
 		@radius = "&radius=" + @radius
-
 	end
 	def search
 		@@api_server_key = "&key=AIzaSyD1-K56-s2HMhsROyTaWbKSl2w2Z4fkKe0"
@@ -51,48 +48,83 @@ class Search
 			SearchResult.new(place)
 		end
 		tp SearchResult.all
+		SearchResult.sort
 	end
 end
-
 class SearchResult
 	@@results = []
-	attr_reader :name, :price, :rating, :address, :open
+	attr_reader :num, :name, :price, :rating, :address, :open
 	def initialize(place)
-			@name = place["name"]
-			if @name == nil
-				@name  = "Unavailable"
-			end
-			@price = place["price_level"]
-			if @price == nil
-				@price  = "Unavailable"
-			end
-			@rating = place["rating"]
-			if @rating == nil
-				@rating  = "Unavailable"
-			end
-			@address = place["vicinity"]
-			if @address == nil
-				@address  = "Unavailable"
-			end
-			if place["opening_hours"] != nil 
-				@open = place["opening_hours"]["open_now"]
-			end 
-			if @open == nil
-				@open = "Unavailable"
-			end
-			@@results << self
+		@num  = @@results.length + 1
+		@name = place["name"]
+		if @name == nil
+			@name  = "Unavailable"
+		end
+		@price = place["price_level"]
+		if @price == nil
+			@price  = "Unavailable"
+		end
+		@rating = place["rating"]
+		if @rating == nil
+			@rating  = "Unavailable"
+		end
+		@address = place["vicinity"]
+		if @address == nil
+			@address  = "Unavailable"
+		end
+		if place["opening_hours"] != nil 
+			@open = place["opening_hours"]["open_now"]
+		end 
+		if @open == nil
+			@open = "Unavailable"
+		end
+		@@results << self
+	end
+	def num=(num)
+		@num= num
 	end
 	def self.all
 		@@results
 	end
+	def self.sort
+		puts "How would you like to sort?"
+		puts "1.Price\n2.Rating\n3.Omit Closed\n4.Done Sorting"
+		how_to_sort = gets.chomp
+		if how_to_sort == "1" or how_to_sort.downcase == "price"
+			@@results.sort_by! do |result|
+				result.price.to_f
+			end
+			@@results.reverse!
+			@@results.each_with_index do |result,index|
+				result.num=(index+1)
+			end
+		elsif how_to_sort == "2" or how_to_sort.downcase == "rating"
+			@@results.sort_by! do |result|
+				result.rating.to_f
+			end
+			@@results.reverse!
+			@@results.each_with_index do |result,index|
+				result.num=(index+1)
+			end
+		elsif how_to_sort == "3" or how_to_sort.downcase == "omit closed"
+			@@results.collect! do |result|
+				if result.open == true
+					result
+				end
+			end
+			@@results.compact!
+			@@results.each_with_index do |result,index|
+				result.num=(index+1)
+			end
+		elsif how_to_sort == "4" or how_to_sort.downcase == "done sorting"
+		puts "In progress..."
+		end
+		unless how_to_sort == "4" or how_to_sort.downcase == "done sorting"
+			tp SearchResult.all
+			self.sort 
+		end
+	end
 end
-
-
-
-
-
-
-
 bob = Location.new
 bob.cord_finder
 charles = Search.new(bob)
